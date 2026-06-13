bd117954fa0bf2673940b2efab8370c7005f581c
joon8129-debug <joon8129@gmail.com>
2026-06-13 09:26:40 +0900

    Add aggregate reusable platform exports

---
+++ kgia-golf-platform-modular/packages/golf-platform/src/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  partnerTest: require('../../partner-test/src'),
+  matching: require('../../matching-engine/src'),
+  gci: require('../../gci-engine/src'),
+  proam: require('../../proam-engine/src'),
+  commerce: require('../../commerce-engine/src'),
+  crm: require('../../crm-engine/src'),
+  growth: require('../../growth-engine/src'),
+  privacy: require('../../privacy-consent/src'),
+  serverKit: require('../../server-kit/src')
+};
